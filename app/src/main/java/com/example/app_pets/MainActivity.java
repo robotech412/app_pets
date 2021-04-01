a57afa72376f5802d5313f72c5c0d4d9fc6d6b5b
Dennis Ake <dariel_504@hotmail.com>
2021-04-01 02:41:37 -0600

    creacion de vista registro de usuario

--- app/src/main/java/com/example/app_pets/MainActivity.java
+++ app/src/main/java/com/example/app_pets/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.app_pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditTextName;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private Button mButtonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditTextName =(EditText) findViewById(R.id.editTextName);
+        mEditTextEmail=(EditText) findViewById(R.id.editTextEmail);
+        mEditTextPassword=(EditText)findViewById(R.id.ediTextPassword);
     }
 }
