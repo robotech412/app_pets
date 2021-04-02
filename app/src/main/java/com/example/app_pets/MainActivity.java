91164467da54605ba267d9d6c64ed3a25a94e473
Dennis Ake <dariel_504@hotmail.com>
2021-04-02 01:04:07 -0600

    anexo funcionalidad de registro de usuarios

--- app/src/main/java/com/example/app_pets/MainActivity.java
+++ app/src/main/java/com/example/app_pets/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.app_pets;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +27,81 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextPassword;
     private Button mButtonRegister;
 
+    //variables de los datos a registrar
+
+    private String name= "";
+    private String password="";
+    private String email="";
+
+     FirebaseAuth mAuth;
+     DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth= FirebaseAuth.getInstance();
+        mDatabase= FirebaseDatabase.getInstance().getReference();
+
         mEditTextName =(EditText) findViewById(R.id.editTextName);
         mEditTextEmail=(EditText) findViewById(R.id.editTextEmail);
         mEditTextPassword=(EditText)findViewById(R.id.ediTextPassword);
+        mButtonRegister=(Button)findViewById(R.id.btnRegister);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name=mEditTextName.getText().toString();
+                password=mEditTextPassword.getText().toString();
+                email=mEditTextEmail.getText().toString();
+
+                if(!name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+                    if(password.length()>=6){
+                        registerUser();
+
+                    }else{
+                        Toast.makeText(MainActivity.this,"El password debe tener al menos 6 caracteres",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else
+                    Toast.makeText(MainActivity.this,"Debe completar los campos",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void registerUser(){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("name",name);
+                    map.put("email",email);
+                    map.put("password",password);
+
+
+                    String id=mAuth.getCurrentUser().getUid();
+
+                    mDatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()){
+                                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this,"No se pudieron crear los datos correctamente",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(MainActivity.this,"No se pudo registrar este usuario",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
