ad3c8de65d68bbbcc6b584a6279ea8449a25b211
DESKTOP-9NJSUEQ <6223@itescam.edu.mx>
2021-04-07 16:08:20 -0500

    Anexo vista registro de medicamentos

---
+++ app/src/main/java/com/example/app_pets/RegistroMedActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroMedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_med);
+    }
+}
