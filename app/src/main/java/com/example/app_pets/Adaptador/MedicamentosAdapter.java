93286e2b1d99d70c8d2650b054dbfdecd7d8fbe9
Dennis Ake <dariel_504@hotmail.com>
2021-06-26 01:43:44 -0500

    added function to eliminate medicines

--- app/src/main/java/com/example/app_pets/Adaptador/MedicamentosAdapter.java
+++ app/src/main/java/com/example/app_pets/Adaptador/MedicamentosAdapter.java
@@ -1,9 +1,12 @@
 package com.example.app_pets.Adaptador;
 
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,8 +15,15 @@ import com.example.app_pets.R;
 import com.example.app_pets.model.Medicamento;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MedicamentosAdapter extends FirestoreRecyclerAdapter<Medicamento,MedicamentosAdapter.ViewHolder> {
+public class MedicamentosAdapter extends FirestoreRecyclerAdapter<Medicamento, MedicamentosAdapter.ViewHolder> {
+
+    Activity activity;
+    FirebaseFirestore db;
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -23,6 +33,7 @@ public class MedicamentosAdapter extends FirestoreRecyclerAdapter<Medicamento,Me
      */
     public MedicamentosAdapter(@NonNull FirestoreRecyclerOptions<Medicamento> options) {
         super(options);
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -32,27 +43,40 @@ public class MedicamentosAdapter extends FirestoreRecyclerAdapter<Medicamento,Me
         viewHolder.textViewCantidad.setText(medicamento.getCantidad());
         viewHolder.textViewUso.setText(medicamento.getUso());
         viewHolder.textViewDescripcion.setText(medicamento.getDescripcion());
+
+        DocumentSnapshot MedicamentoDocument = getSnapshots().getSnapshot(viewHolder.getAdapterPosition());
+
+        final String id = MedicamentoDocument.getId();
+
+        viewHolder.btnElininar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Medicamentos").document(id).delete();
+            }
+        });
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_medicamentos,viewGroup,false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_medicamentos, viewGroup, false);
         return new ViewHolder(view);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textViewMarca,textViewNombre,textViewCantidad,textViewUso,textViewDescripcion;
+        TextView textViewMarca, textViewNombre, textViewCantidad, textViewUso, textViewDescripcion;
+        Button btnElininar;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            textViewMarca=itemView.findViewById(R.id.textViewMarca);
-            textViewNombre=itemView.findViewById(R.id.textViewNombre);
-            textViewCantidad=itemView.findViewById(R.id.textViewCantidad);
-            textViewUso=itemView.findViewById(R.id.textViewUso);
-            textViewDescripcion=itemView.findViewById(R.id.textViewDescripcion);
+            textViewMarca = itemView.findViewById(R.id.textViewMarca);
+            textViewNombre = itemView.findViewById(R.id.textViewNombre);
+            textViewCantidad = itemView.findViewById(R.id.textViewCantidad);
+            textViewUso = itemView.findViewById(R.id.textViewUso);
+            textViewDescripcion = itemView.findViewById(R.id.textViewDescripcion);
+            btnElininar = itemView.findViewById(R.id.btn_eliminar_med);
         }
     }
 
